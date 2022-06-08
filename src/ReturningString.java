a76d82627191e255bba0d5aa6f09a511c97768f6
Segun Osiki <Osiki.segun@gmail.com>
2022-06-08 11:35:34 +0100

    add files

---
+++ src/ReturningString.java
@@ -0,0 +1,21 @@
+
+//Make a function that will return a greeting statement that uses an input; your program should return,
+//        "Hello, <name> how are you doing today?".
+//
+//        [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+
+public class ReturningString {
+
+    public static void main(String[] args) {
+        System.out.println(greet("Ryan"));
+    }
+    public static String greet(String name)
+    {
+        // Your code here
+        return "Hello" + ", " + name + " " + "how are you doing today?";
+    }
+
+
+}
