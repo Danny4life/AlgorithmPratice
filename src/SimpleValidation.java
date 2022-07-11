18c5f807e2c777690c1753f6de8ef50cc1e0eb02
segun osiki <osiki.segun@gmail.com>
2022-07-11 18:39:08 +0100

    adding files

---
+++ src/SimpleValidation.java
@@ -0,0 +1,22 @@
+//Write a simple regex to validate a username. Allowed characters are:
+//
+//        lowercase letters,
+//        numbers,
+//        underscore
+//        Length should be between 4 and 16 characters (both included).
+//
+
+
+public class SimpleValidation {
+    public static void main(String[] args) {
+        System.out.println(validateUsr("reg"));
+    }
+
+    public static boolean validateUsr(String s) {
+        //glhf:)
+
+
+       return s.matches("[a-z_\\d]{4,16}");
+
+    }
+}
